4d0225dccef8930c4364947ecbd5ae8f2aa9f07d
Shaswat Satyam <shaswatsatyam51@gmail.com>
2025-07-30 11:48:11 +0000

    Correct the REST API routes

--- server/index.js
+++ server/index.js
@@ -1,86 +1,111 @@
-const express = require("express");
-const fs = require("fs");
-const cors = require("cors");
+import express from "express";
+import fs from "fs";
+import cors from "cors";
+import openDb from "./server.js";
+import { title } from "process";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+const db = await openDb();
 
 console.log("App Started");
 
-let mediaData = [];
-
-function readSavedData() {
-  const file = "data.json";
-  data = fs.readFile(file, "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    try {
-      res = JSON.parse(data);
-      res.forEach((element) => {
-        mediaData.push(element);
-      });
-    } catch (parseErr) {
-      console.error("Error parsing JSON data:", parseErr);
-    }
-  });
-}
-
-readSavedData();
-
 app.get("/", (req, res) => {
-  res.send({ hello: "world" });
-});
-
-app.get("/refresh", (req, res) => {
-  const file = "output.json";
-  data = fs.readFile(file, "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    try {
-      mediaData = JSON.parse(data);
-    } catch (parseErr) {
-      console.error("Error parsing JSON data:", parseErr);
-      res.send({ Error: "Error Parsing JSON data" });
-    }
+  res.send({
+    title: "This is an API to server JSON data for various movies on Youtube",
   });
-  res.send("Done");
 });
 
-app.get("/data", (req, res) => {
-  res.send(mediaData);
-});
+async () => {
+  const db = await openDb();
+  await db.exec(`
+        CREATE TABLE IF NOT EXISTS movies (
+        imdb STRING PRIMARY KEY ,
+        name TEXT,
+        ytId TEXT
+      );
+      `);
+};
 
-app.get("/add", (req, res) => {
-  const imdbKey = req.query.imdbKey;
-  const type = req.query.type;
-  const id = req.query.id;
-  const name = req.query.name;
-
-  if (
-    (imdbKey != undefined) &
-    (type != undefined) &
-    (id != undefined) &
-    (name != undefined)
-  ) {
-    mediaData.append({
-      imdb: imdbKey,
-      name: name,
-      type: type,
-      ytId: id,
-    });
-    res.send("Added a new item");
-  } else {
-    res.send("Please provide imdbKey, type, id and name");
+app.get("/data", async (req, res) => {
+  try {
+    const movies = await db.all("SELECT * FROM movies");
+    movies.forEach((movie) => (movie["type"] = "movie"));
+    res.json(movies);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
-app.get("/save", (req, res) => {
-  var writer = fs.createWriteStream("output.json");
-  writer.write(JSON.stringify(mediaData));
+app.get("/movies", async (_, res) => {
+  try {
+    const movies = await db.all("SELECT * FROM movies");
+    res.json(movies);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.delete("/movies", async (req, res) => {
+  const { imdb, pass } = req.query;
+  try {
+    if (!imdb || !pass || pass != "password") {
+      res.status(500).json({ error: "Incomplete Data: imdb and pass" });
+    } else {
+      const result = await db.run("DELETE FROM movies where imdb = ?", imdb);
+      res.status(201).json({ id: result.lastID, imdb });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/movies", async (req, res) => {
+  const { imdb, name, ytId } = req.query;
+  try {
+    if (!imdb || !name || !ytId) {
+      Error("Incomplete Data: imdb, name, ytId");
+    }
+    const result = await db.run(
+      "INSERT INTO movies (imdb, name, ytId) VALUES (?, ?, ?)",
+      imdb,
+      name,
+      ytId
+    );
+    res.status(201).json({ id: result.lastID, imdb, name, ytId });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.patch("/movies", async (req, res) => {
+  const { imdb, name, ytId } = req.query;
+  try {
+    if (!imdb && (!name || !ytId)) {
+      res.status(201).json({ Error: "Incomplete Data: imdb name, ytId" });
+    }
+    if (name) {
+      const result = await db.run(
+        "UPDATE TABLE movies SET name = ? WHERE imdb = ?",
+        name,
+        imdb
+      );
+
+      res.status(201).json({ id: result.lastID, imdb, name, ytId });
+    } else {
+      const result = await db.run(
+        "UPDATE movies SET ytId = ? WHERE imdb = ?",
+        ytId,
+        imdb
+      );
+
+      res.status(201).json({ id: result.lastID, imdb, name, ytId });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 const port = 8080;
