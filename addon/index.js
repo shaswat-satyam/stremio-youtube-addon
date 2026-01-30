d83883d29e86c019dcc7a1231aa9ee02e7495750
Shaswat Satyam <70892928+shaswat-satyam@users.noreply.github.com>
2026-01-30 19:11:02 +0530

    Updating addons

--- addon/index.js
+++ addon/index.js
@@ -1,5 +1,7 @@
 const { addonBuilder, serveHTTP } = require("stremio-addon-sdk");
-const env = require("env");
+require("dotenv").config();
+
+const BaseAPIurl = process.env.BaseAPIurl;
 
 const manifest = {
   id: "org.stremio.youtube-media",
@@ -11,64 +13,157 @@ const manifest = {
   catalogs: [
     {
       type: "movie",
-      id: "helloworldmovies",
+      id: "Youtube-Media-Movies",
     },
     {
       type: "series",
-      id: "helloworldseries",
+      id: "Youtube-Media-Series",
+    },
+    {
+      type: "tv",
+      id: "Youtube-Media-tv",
     },
   ],
-  idPrefixes: ["tt"],
+  idPrefixes: ["tt", "youtube-tv-channel-"],
 };
 
 const builder = new addonBuilder(manifest);
-const baseAPIurl = env["baseAPIurl"];
 
 const METAHUB_URL = "https://images.metahub.space";
 
-const generateMetaPreview = function (value, key) {
+const generateMetaPreview = function (imdb_id, name, type) {
   // To provide basic meta for our movies for the catalog
   // we'll fetch the poster from Stremio's MetaHub
   // see https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/meta.md#meta-preview-object
 
-  getData();
-  const imdbId = key.split(":")[0];
+  if (type == "tv") {
+    return {
+      id: imdb_id,
+      type: type,
+      name: name,
+    };
+  }
+
   return {
-    id: imdbId,
-    type: value.type,
-    name: value.name,
-    poster: METAHUB_URL + "/poster/medium/" + imdbId + "/img",
+    id: imdb_id.split(":")[0],
+    type: type,
+    name: name,
+    poster: METAHUB_URL + "/poster/medium/" + imdb_id + "/img",
   };
 };
 
-builder.defineCatalogHandler(function (args, cb) {
-  // filter the dataset object and only take the requested type
-  const metas = Object.entries(data)
-    .filter(([_, value]) => value.type === args.type)
-    .map(([key, value]) => generateMetaPreview(value, key));
+builder.defineMetaHandler(async ({ id, type }) => {
+  return {
+    meta: {
+      id,
+      type,
+      name: id,
+    },
+  };
+});
 
+builder.defineCatalogHandler(async (args, cb) => {
+  // filter the dataset object and only take the requested type
+  let result = await getStreamsFromDatabase(args.type);
+  let metas;
+  if (result.length > 0) {
+    if (args.type == "tv") {
+      console.log(result);
+      metas = result.map(({ id, name }) =>
+        generateMetaPreview("youtube-tv-channel-" + id, name, args.type),
+      );
+    } else {
+      metas = result.map(({ imdb_id, name }) =>
+        generateMetaPreview(imdb_id, name, args.type),
+      );
+    }
+  }
   return Promise.resolve({ metas: metas });
 });
 
-async function getStreamsFromDatabase(id, type) {
-  const response = await fetch(`${ApiURL}/${type}?id=${id}`);
+async function getStreamsFromDatabase(type) {
+  let response;
+  if (type == "series") {
+    response = await fetch(`${BaseAPIurl}/${type}`);
+  } else {
+    response = await fetch(`${BaseAPIurl}/${type}s`);
+  }
+  if (!response.ok) {
+    console.error("Failed to fetch streams:", response.statusText);
+    return [];
+  }
+
+  const result = await response.json();
+
+  if (type == "tv") {
+    return result.data.map((r) => ({
+      id: r.id,
+      poster: r.poster
+        ? r.poster
+        : "https://www.lifeinabreakdown.com/wp-content/uploads/2023/01/older-television-on-spindly-legs-with-large-dials-image-licensed-via-canva-pro-and-copyright-via-Getty-Images-Signature-and-shaunl.jpg",
+      type: "tv",
+      name: r.name,
+    }));
+  }
+  return result.data.map((r) => ({
+    youtube_id: r.youtube_id,
+    imdb_id: r.imdb_id,
+    name: r.name,
+  }));
+}
+async function getStreamsFromDatabaseForParticularId(id, type, name) {
+  let response;
+  if (type == "movie" || type == "series") {
+    response = await fetch(`${BaseAPIurl}/${type}/${id}`);
+  } else if (type == "tv") {
+    response = await fetch(`${BaseAPIurl}/${type}/${id.split("-").at(-1)}`);
+    if (!response.ok) {
+      console.error("Failed to fetch streams:", response.statusText);
+      return [];
+    }
+    let result = await response.json();
+    return {
+      id: id,
+      title: result.data.title,
+      name: result.data.name,
+      youtube_id: result.data.youtube_id,
+    };
+  }
 
   if (!response.ok) {
     console.error("Failed to fetch streams:", response.statusText);
     return [];
   }
 
-  const results = await response.json();
+  const result = await response.json();
 
-  return results.map((r) => ({
-    id: r._id.toString(),
+  return result.data.map((r) => ({
+    id: r.id.split("-").at(-1),
+    title: "Live Stream",
     youtube_id: r.youtube_id,
     imdb: r.imdb_id,
   }));
 }
 
 builder.defineStreamHandler(async ({ id, type }) => {
-  const streamsFromDB = await getStreamsFromDatabase(id, type);
+  let streamsFromDB;
+  if (type == "tv") {
+    streamsFromDB = await getStreamsFromDatabaseForParticularId(id, type);
+    return {
+      id: id,
+      title: "from youtube " + type,
+      ytId: streamsFromDB.youtube_id,
+      name: streamsFromDB.name,
+      type: "tv",
+      name: streamsFromDB.title,
+      behaviorHints: {
+        notWebReady: true,
+      },
+    };
+  } else {
+    streamsFromDB = await getStreamsFromDatabaseForParticularId(id, type);
+  }
+
   const streams = streamsFromDB.map((stream) => ({
     id: stream.id,
     imdb: stream.imdb_id,
